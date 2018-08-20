542133bd9702d414a5ebf60e1f6f26da032b0563
Nuruddin Ashr <uudashr@gmail.com>
2018-08-20 14:08:08 +0700

    Add id on Course

--- internal/course/course.go
+++ internal/course/course.go
@@ -2,14 +2,21 @@ package course
 
 // Course represents course.
 type Course struct {
+	id   string
 	name string
 }
 
 // New creates new Course.
-func New(name string) (*Course, error) {
+func New(id, name string) (*Course, error) {
 	return &Course{
+		id:   id,
 		name: name,
 	}, nil
+}
+
+// ID of the course.
+func (c Course) ID() string {
+	return c.id
 }
 
 // Name of the course.
