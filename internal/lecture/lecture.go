c3e6ee01b5cb825777baccc451a0c1458c111912
Nuruddin Ashr <uudashr@gmail.com>
2018-08-20 14:07:15 +0700

    Add id on Lecture

--- internal/lecture/lecture.go
+++ internal/lecture/lecture.go
@@ -2,13 +2,15 @@ package lecture
 
 // Lecture represents lecture.
 type Lecture struct {
+	id       string
 	title    string
 	videoURL string
 }
 
 // New constructs new Lecture.
-func New(title, videoURL string) (*Lecture, error) {
+func New(id, title, videoURL string) (*Lecture, error) {
 	return &Lecture{
+		id:       id,
 		title:    title,
 		videoURL: videoURL,
 	}, nil
